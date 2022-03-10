36c90b20cfde0198ee19b960a840fbdd29d1aa69
zhengow <38389586+zhengow@users.noreply.github.com>
2022-03-10 10:34:04 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ define( 'DVWA_WEB_PAGE_TO_ROOT', '' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'authenticated', 'phpids' ) );
-
+browntest
 $page = dvwaPageNewGrab();
 $page[ 'title' ]   = 'Welcome' . $page[ 'title_separator' ].$page[ 'title' ];
 $page[ 'page_id' ] = 'home';
