56e92fd73acfd0b3ce9d3811b7211f85094cf929
zhengow <38389586+zhengow@users.noreply.github.com>
2022-03-10 20:07:36 +0800

    Update low.php

--- vulnerabilities/brute/source/low.php
+++ vulnerabilities/brute/source/low.php
@@ -10,7 +10,7 @@ if( isset( $_GET[ 'Login' ] ) ) {
 
 	// Check the database
 	$query  = "SELECT * FROM `users` WHERE user = '$user' AND password = '$pass';";
-	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
+	$result = testesteli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '</pre>' );
 
 	if( $result && mysqli_num_rows( $result ) == 1 ) {
 		// Get users details
