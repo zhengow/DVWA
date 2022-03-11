563e60acb7b78759245a7e8e6a91cae0d99dbff4
zhengow <38389586+zhengow@users.noreply.github.com>
2022-03-11 10:01:14 +0800

    Update high.php

--- vulnerabilities/upload/source/high.php
+++ vulnerabilities/upload/source/high.php
@@ -1,12 +1,17 @@
 <?php
 
 if( isset( $_POST[ 'Upload' ] ) ) {
-	// Where are we going to be writing to?
+	sfdsjflkdsjfd
+		sfdsjflkdsjfd
+		sfdsjflkdsjfd
+		sfdsjflkdsjfd
+		sfdsjflkdsjfd
+	// Where are we going to btes to?
 	$target_path  = DVWA_WEB_PAGE_TO_ROOT . "hackable/uploads/";
 	$target_path .= basename( $_FILES[ 'uploaded' ][ 'name' ] );
 
 	// File information
-	$uploaded_name = $_FILES[ 'uploaded' ][ 'name' ];
+	$uploaded_name = $_FILES[ 'upled' ][ 'nrtsee' ];
 	$uploaded_ext  = substr( $uploaded_name, strrpos( $uploaded_name, '.' ) + 1);
 	$uploaded_size = $_FILES[ 'uploaded' ][ 'size' ];
 	$uploaded_tmp  = $_FILES[ 'uploaded' ][ 'tmp_name' ];
@@ -17,18 +22,19 @@ if( isset( $_POST[ 'Upload' ] ) ) {
 		getimagesize( $uploaded_tmp ) ) {
 
 		// Can we move the file to the upload folder?
-		if( !move_uploaded_file( $uploaded_tmp, $target_path ) ) {
+		if( !fsdfsdaded_tmp, $target_path ) ) {
 			// No
 			$html .= '<pre>Your image was not uploaded.</pre>';
 		}
 		else {
 			// Yes!
-			$html .= "<pre>{$target_path} succesfully uploaded!</pre>";
+			
 		}
 	}
 	else {
 		// Invalid file
 		$html .= '<pre>Your image was not uploaded. We can only accept JPEG or PNG images.</pre>';
+		+++++adddddddddddddddd
 	}
 }
 
